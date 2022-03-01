bbf5f31c91f3b956feac8f3ca32a503c50a396e5
Juste Raimbault <juste.raimbault@polytechnique.edu>
2022-03-01 12:31:22 +0000

    add_data_energyprice

--- 2-StatistiquesSpatiales/TP/AS-spatialstats.R
+++ 2-StatistiquesSpatiales/TP/AS-spatialstats.R
@@ -14,17 +14,91 @@
 #'  * pas de header
 #'  * utiliser de préférence dplyr et readr (tidyverse) 
 
+library(dplyr)
+library(readr)
+
 # 1.1) charger les données
 #  Question : système de coordonnées?
 
+coords <- read_csv("data/FRUrbanAreas/coords.csv",col_names = F)
+colnames(coords)<-c("name","x","y")
+  
+populations <- read_csv("data/FRUrbanAreas/pop50.csv",col_names = F)
+dates <- read_csv("data/FRUrbanAreas/dates.csv", col_names = F)
+
+colnames(populations)<- as.character(dates$X1)
+
+# coordonnees coherentes ?
+plot(coords$x,coords$y)
+
+# population totale par annee?
+totalpop = colSums(populations)
+plot(dates$X1,totalpop,type='l')
+points(dates$X1,populations[1,],type='l',col='red')
 
 # 1.2) calculer point moyen, point moyen pondéré, distance-type pour chaque date
 
+# point moyen
+meanx = mean(coords$x)
+meany = mean(coords$y)
+meanpoint = apply(coords[,c("x","y")],MARGIN = 2, mean)
+
+plot(coords$x,coords$y);points(meanx,meany,col='red')
+
+
+# point moyen pondere
+
+# pour 1831
+wmeanx1831 = sum(populations$`1831`*coords$x)/sum(populations$`1831`)
+wmeany1831 = sum(populations$`1831`*coords$y)/sum(populations$`1831`)
+
+# boucle pour chaque annee
+wmean = list()
+for(date in as.character(dates$X1)){
+  show(date)
+  wmeanx = sum(populations[,date]*coords$x)/sum(populations[,date])
+  wmeany = sum(populations[,date]*coords$y)/sum(populations[,date])
+  wmean[[date]] = c(wmeanx,wmeany)
+}
+
+# meme chose avec apply
+wmean = apply(populations,MARGIN = 2,FUN = function(pop){
+  currenttotalpop = sum(pop)
+  wmeanx = sum(pop*coords$x)/currenttotalpop
+  wmeany = sum(pop*coords$y)/currenttotalpop
+  return(c(wmeanx,wmeany))
+}
+)
+wmean=t(wmean)
+colnames(wmean)=c("x","y")
+wmean=data.frame(wmean)
+wmean$date = dates$X1
 
 # 1.3) cartographier les villes et l'évolution du point moyen
-#  Question : quel package pour cartographier "simplement" ?
+#  Question : quel package pour cartographier "simplement" ? -> ggplot (function geom_sf)
 #  Données supplémentaires: limites des régions: data/regions/regions_2015_metropole_region.shp
 #   ! systèmes de coordonnées à gérer
+
+library(sf)
+library(ggplot2)
+
+# voir les drivers disponibles (formats de fichiers)
+st_drivers()
+
+regions <- st_read(dsn='data/regions/',layer='regions_2015_metropole_region')
+
+# plot point moyen uniquement
+g=ggplot(data=wmean,aes(x=x,y=y,col=date))
+g+geom_point()
+
+# "carte" avec point moyen et regions
+g=ggplot(data=regions)
+g+geom_sf()+geom_point(data=wmean,aes(x=x*100,y=y*100,col=date))
+
+# meme carte "zoomee"
+g=ggplot(data=regions[regions$RégION%in%c("Bourgogne et Franche-Comté","Centre"),])
+g+geom_sf()+geom_point(data=wmean,aes(x=x*100,y=y*100,col=date))
+
 
 
 # 1.4) faire de même avec le point médian et point médian pondéré
